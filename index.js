b5c2bda62e2e647d76eff7cff21e8167e7d3e47c
Easwari Ramani <eshuprad@gmail.com>
2017-12-14 10:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Write your solution in this file!
 const driver={};
+function updateDriverWithKeyAndValue(driver, key, value){
+
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+
+}
+function deleteFromDriverByKey(driver, key){
+
+}
+function destructivelyDeleteFromdriverByKey(driver, key){
+
+}
