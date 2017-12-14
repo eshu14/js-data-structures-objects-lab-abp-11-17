5cfda9f4b68e6280e92a9fc4520c64a4130e8e2e
Easwari Ramani <eshuprad@gmail.com>
2017-12-14 11:04:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
   return(Object.assign(newObj,driver,{[key]:value}));
 }
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  return(Object.assign(driver,{key,value}));
+  return(Object.assign(driver,{[key]:value}));
 }
 function deleteFromDriverByKey(driver, key){
   Object.assign(newObj,driver);
